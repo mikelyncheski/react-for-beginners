4b0a7948221f2feeb1c8180847ff1f455cc0532c
Mike Lyncheski <mll@harriergroup.com>
2018-03-17 08:07:17 -0400

    Some more prettier setting changes

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,16 +1,23 @@
 import React from "react";
 import AddFishForm from "./AddFishForm";
+import EditFishForm from "./EditFishForm";
 
 /* eslint-disable react/prop-types */
 class Inventory extends React.Component {
 	render() {
 		return (
 			<div className="Inventory">
-				Inventory!!
+				<h2>Inventory!!</h2>
+				{Object.keys(this.props.fishes).map(fishKey => (
+					<EditFishForm
+						key={fishKey}
+						fishKey={fishKey}
+						fish={this.props.fishes[fishKey]}
+						updateFish={this.props.updateFish}
+					/>
+				))}
 				<AddFishForm x={this.props.age} addFish={this.props.addFish} />
-				<button onClick={this.props.loadSampleFishes}>
-					Load Samples Fishes
-				</button>
+				<button onClick={this.props.loadSampleFishes}>Load Samples Fishes</button>
 			</div>
 		);
 	}
